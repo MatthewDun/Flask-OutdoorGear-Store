43f653d4129f1a2bfae1609b4f6c500ea25d662d
MatthewDun <matthewwdunnavant@gmail.com>
2025-07-14 23:05:04 -0700

    updated login.html and app.py. Added venv

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, redirect, request, url_for
+
+app = Flask(__name__)
+
+
+
+@app.route("/", methods=["GET", "POST"])
+
+def home():
+    return render_template("login.html")
+
+
+
+@app.route("/store")
+
+def store():
+    return render_template("store.html")
+
+
+
+@app.route("/submit-form", methods=["GET", "POST"])
+
+def authenticate():
+    Username = request.form["Username"]
+    Password = request.form["Password"]
+
+    print("Username is", Username)
+    if Username == "Admin" and Password == "Pass":
+        return redirect(url_for('store'))
+    else:
+        return redirect(url_for('home'))
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
